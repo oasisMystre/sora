80d7c69adfd52aabb072f04b3bb13ecdbc115d64
Include caleb.fun <70612830+oasisMystre@users.noreply.github.com>
2024-02-25 07:51:26 -0800

    Update get.scene.ts

--- src/scenes/get.scene.ts
+++ src/scenes/get.scene.ts
@@ -50,7 +50,7 @@ export const getVideoScene = new Scenes.WizardScene<Scenes.WizardContext>(
     ctx.wizard.next();
   },
   async (ctx) => {
-    await sleep(30000);
+    await sleep(10000);
 
     const message = ctx.message as any;
     const response = await Api.instance.video.getVideo(message.text);
